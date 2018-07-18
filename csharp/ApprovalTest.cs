d1bba9ff04d9b6fa51557d00846f8607e60b9ed2
Robin Leach <Robin.Leach@softwire.com>
2018-07-18 12:23:01 +0100

    Added all the tests for the current versions functionality

--- csharp/ApprovalTest.cs
+++ csharp/ApprovalTest.cs
@@ -14,13 +14,6 @@ namespace csharp
         [Test]
         public void ThirtyDays()
         {
-            StringBuilder fakeoutput = new StringBuilder();
-            Console.SetOut(new StringWriter(fakeoutput));
-            Console.SetIn(new StringReader("a\n"));
-
-            Program.Main(new string[] { });
-            String output = fakeoutput.ToString();
-            Approvals.Verify(output);
         }
     }
 }
--- csharp/GildedRoseTest.cs
+++ csharp/GildedRoseTest.cs
@@ -7,12 +7,80 @@ namespace csharp
     public class GildedRoseTest
     {
         [Test]
-        public void foo()
+        public void IfPastSellByDate_QualityDegradesTwiceAsFast()
         {
-            IList<Item> Items = new List<Item> { new Item { Name = "foo", SellIn = 0, Quality = 0 } };
+            IList<Item> Items = new List<Item> {
+                new Item { Name = "test_item_1", SellIn = 10, Quality = 10 },
+                new Item { Name = "test_item_2", SellIn = 0, Quality = 10 }
+            };
             GildedRose app = new GildedRose(Items);
             app.UpdateQuality();
-            Assert.AreEqual("fixme", Items[0].Name);
+            Assert.AreEqual(Items[0].Quality, Items[1].Quality+1);
+        }
+
+        [Test]
+        public void QualityNeverNegative()
+        {
+            IList<Item> Items = new List<Item> {
+                new Item { Name = "test_item", SellIn = 10, Quality = 0 },
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.GreaterOrEqual(Items[0].Quality,0);
+        }
+
+        [Test]
+        public void AgedBrieGainsQuality()
+        {
+            IList<Item> Items = new List<Item> {
+                new Item { Name = "Aged Brie", SellIn = 10, Quality = 10 },
+                new Item { Name = "Aged Brie", SellIn = 0, Quality = 10 },
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(Items[0].Quality,11);
+            Assert.AreEqual(Items[1].Quality, 12);
+        }
+
+        [Test]
+        public void QualityNeverMoreThanFifty()
+        {
+            IList<Item> Items = new List<Item> {
+                new Item { Name = "Aged Brie", SellIn = 10, Quality = 50 },
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(Items[0].Quality, 50);
+        }
+
+        [Test]
+        public void SulfurasNeverChangesQuality()
+        {
+            IList<Item> Items = new List<Item> {
+                new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 10, Quality = 80 },
+                new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = -1, Quality = 80 }
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(Items[0].Quality, 80);
+            Assert.AreEqual(Items[1].Quality, 80);
+        }
+
+        [Test]
+        public void BackStagePassesIncreaseInQualityBeforeSellIn()
+        {
+            IList<Item> Items = new List<Item> {
+                new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 20, Quality = 10 },
+                new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 10, Quality = 10 },
+                new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = 5, Quality = 10 },
+                new Item { Name = "Backstage passes to a TAFKAL80ETC concert", SellIn = -5, Quality = 10 }
+            };
+            GildedRose app = new GildedRose(Items);
+            app.UpdateQuality();
+            Assert.AreEqual(Items[0].Quality, 11);
+            Assert.AreEqual(Items[1].Quality, 12);
+            Assert.AreEqual(Items[2].Quality, 13);
+            Assert.AreEqual(Items[3].Quality, 0);
         }
     }
 }
